126e21f60803dec779148e7f0008c3eeb96b684b
zgoodman1 <zgoodman@live.unc.edu>
2018-10-09 12:56:53 -0400

    still issues to be resolved but this is a general idea of what we want, it has a random picture as a placeholder for an article picture, the article title, and it's supposed to have the article description as well but I haven't figured out how to work out the kinks with that yet

--- GeneralBuild.py
+++ GeneralBuild.py
@@ -27,37 +27,41 @@ img = tk.Label(root, image=bgImg)
 # bc this is a background photo, can just pack it in
 img.pack(side="top", fill="both", expand=True)
 
-
-# blueBg creates a Carolina blue canvas overlayed on the bgImg
-# bg is background and the hex code within represents the id for
-# UNC blue, or at least very close to it
-# height and width were based on whatever the size of the display
-# screen is * .65 or .7 so that the bgImg can be seen as a sort of border
-blueBg = Canvas(root, bg='#4B9CD3', width=screenWidth*.85, height=screenHeight*.75, highlightthickness=0)
-
 # using the place geometry manager, the canvas can be placed relative
 # to the overall window (i just googled and apparently .5 works,
 # I assume bc it places the center of the canvas halfway down and
 # halfway across the window) and anchor places the canvas in a
 # centered position relative to the window itself
-blueBg.place(relx=0.5, rely=0.5, anchor=CENTER)
+# blueBg.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-# create first of three new canvases
+# create three canvases for pic, title, and description
 site1 = feedparser.parse('https://krebsonsecurity.com/feed')
 entry1 = site1.entries[0]
 post1 = entry1.title + "  "
-titleLabel = Label(root, bg="red", width=int(screenWidth*.0472), height=int(screenHeight*.025), text=post1, anchor=S)
-titleLabel.place(x=screenWidth*.075, y=screenHeight*.063)
-tk.Misc.lift(titleLabel)
 
+# trying to create frames now, hopefully can fill frames with labels easier and then just have a frame per site
+blueFrame = Frame(root, bg='#4B9CD3', width=int(screenWidth*.85),
+                  height=int(screenHeight*.757))
+blueFrame.place(relx=.5, rely=.5, anchor=CENTER)
 
+infoFrame = Frame(blueFrame, width=screenWidth*.765, height=screenHeight*.6813)
+infoFrame.place(relx=.5, rely=.5, anchor=CENTER)
+infoFrame.pack_propagate(False)
 
+extraFrame1 = Frame(infoFrame, width=screenWidth*.765, height=screenHeight*.6813)
+extraFrame1.pack(side="top")
 
+extraFrame2 = Frame(infoFrame, width=screenWidth*.765, height=screenHeight*.6813)
+extraFrame2.pack(side="bottom")
 
-
-
-
-
+picLabel = Label(extraFrame1, width=int(screenWidth*.3826),
+                 height=int(screenHeight*.34065), image=bgImg).pack(side="left",
+                                                                    fill="both", expand=True)
+titleLabel = Label(extraFrame1, bg="red", width=int(screenWidth*.3826),
+                   height=int(screenHeight*.34065), text=post1).pack()
+descLabel = Label(extraFrame1, bg="green", width=int(screenWidth * .765),
+                  height=int(screenHeight * .3826), text=entry1.description)\
+    .pack(side="bottom")
 
 # mainloop to keep the window running until it is manually closed
 root.mainloop()
